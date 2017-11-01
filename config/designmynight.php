609b90b4e53d284fcc838383fb1e2b8b402cab1a
Steve Porter <steve@designmynight.com>
2017-11-01 17:59:07 +0000

    feat: add laravel service provider

---
+++ config/designmynight.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'base_url' => env('DESIGNMYNIGHT_BASE_URL', 'https://api.designmynight.com'),
+    'api_version' => env('DESIGNMYNIGHT_API_VERSION', 'v4'),
+    'user_id' => env('DESIGNMYNIGHT_USER_ID'),
+    'api_key' => env('DESIGNMYNIGHT_API_KEY')
+];
